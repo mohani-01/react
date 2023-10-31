70cb13ce0e9c935e47e80c9c74873a97e19bfda6
Nahom <nahomohani@gmail.com>
2023-10-31 16:05:20 +0300

    root.render

--- script.jsx
+++ script.jsx
@@ -1,5 +1,13 @@
+import { crateRoot, createRoot } from 'react-dom/client';
+
+
+
+
 function MyButton() {
     return (
         <button>I'm a button.</button>
     )
 }
+
+const root = createRoot(document.getElementById('root'));
+root.render(< MyButton />)
